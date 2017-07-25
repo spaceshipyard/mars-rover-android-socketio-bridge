f2874cc922dc81eeb6cec24ef89b01527e06345f
Denis Rykovanov <drykovanov@wiley.com>
2017-07-25 23:59:19 +0300

    [+] tracing current status

--- app/src/main/java/com/chaoslabgames/mars/bridge/remote/socketio/MainActivity.java
+++ app/src/main/java/com/chaoslabgames/mars/bridge/remote/socketio/MainActivity.java
@@ -1,115 +1,95 @@
-package com.chaoslabgames.mars.bridge.remote;
+package com.chaoslabgames.mars.bridge.remote.socketio;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
+import android.widget.EditText;
+
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
-import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
 
     BluetoothSocket clientSocket;
 
     final String BT_MAC = "98:D3:31:FC:43:A1";
+    final String DISPATCHER_URL = "http://micro-conf.xyz";
 
+    EditText logText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        logText = (EditText) findViewById(R.id.logText);
+
         //BT
-        //Включаем bluetooth. Если он уже включен, то ничего не произойдет
         String enableBT = BluetoothAdapter.ACTION_REQUEST_ENABLE;
         startActivityForResult(new Intent(enableBT), 0);
 
-        //Мы хотим использовать тот bluetooth-адаптер, который задается по умолчанию
         BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
 
-        //Пытаемся проделать эти действия
-        try{
-            //Устройство с данным адресом - наш Bluetooth Bee
-            //Адрес опредеяется следующим образом: установите соединение
-            //между ПК и модулем (пин: 1234), а затем посмотрите в настройках
-            //соединения адрес модуля. Скорее всего он будет аналогичным.
+        try {
             BluetoothDevice device = bluetooth.getRemoteDevice(BT_MAC);
 
-            //Инициируем соединение с устройством
             Method m = device.getClass().getMethod(
-                    "createRfcommSocket", new Class[] {int.class});
+                    "createRfcommSocket", new Class[]{int.class});
 
             clientSocket = (BluetoothSocket) m.invoke(device, 1);
             clientSocket.connect();
-
-            //В случае появления любых ошибок, выводим в лог сообщение
-        } catch (IOException e) {
+            printMessageOnScreen("CONNECTED to BT");
+        } catch (Exception e) {
             Log.d("BLUETOOTH", e.getMessage());
-        } catch (SecurityException e) {
-            Log.d("BLUETOOTH", e.getMessage());
-        } catch (NoSuchMethodException e) {
-            Log.d("BLUETOOTH", e.getMessage());
-        } catch (IllegalArgumentException e) {
-            Log.d("BLUETOOTH", e.getMessage());
-        } catch (IllegalAccessException e) {
-            Log.d("BLUETOOTH", e.getMessage());
-        } catch (InvocationTargetException e) {
-            Log.d("BLUETOOTH", e.getMessage());
+            printMessageOnScreen("BLUETOOTH connection error " + e);
         }
 
-        //Выводим сообщение об успешном подключении
-        Toast.makeText(getApplicationContext(), "CONNECTED to BT", Toast.LENGTH_LONG).show();
         //end BT
 
-
-
-
         try {
-            final Socket socket = IO.socket("http://micro-conf.xyz");
+            final Socket socket = IO.socket(DISPATCHER_URL);
 
             socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
 
                 @Override
                 public void call(Object... args) {
-                    //socket.emit("foo", "hi");
-                    //socket.disconnect();
-
-                    //Toast.makeText(getApplicationContext(), "CONNECTED to SOCKET IO", Toast.LENGTH_LONG);
+                    printMessageOnScreen("socket io connected");
                 }
 
             }).on("event", new Emitter.Listener() {
 
                 @Override
                 public void call(Object... args) {
-                    JSONObject obj = (JSONObject)args[0];
+                    JSONObject obj = (JSONObject) args[0];
                     Log.d("event", obj.toString());
+                    printMessageOnScreen("event " + obj.toString());
                 }
 
             }).on("message", new Emitter.Listener() {
 
                 @Override
                 public void call(Object... args) {
-                    final JSONObject obj = (JSONObject)args[0];
+                    final JSONObject obj = (JSONObject) args[0];
                     Log.d("message", obj.toString());
                     try {
                         final String cmd = obj.get("cmd").toString();
                         forwardCmd(cmd);
+                        printMessageOnScreen("in-cmd: " + cmd);
                     } catch (JSONException e) {
                         e.printStackTrace();
                         Log.d("socket", "fail to get cmd", e);
@@ -122,13 +102,15 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void call(Object... args) {
                     Log.d("disconnected", args.toString());
+                    printMessageOnScreen("socket is disconnected");
                 }
 
             }).on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
 
                 @Override
-                public void call(Object... args) {
-                    Log.d("socket", "error"+args.toString());
+                public void call(final Object... args) {
+                    Log.d("socket", "error " + args.toString());
+                    printMessageOnScreen("socket error " + args.toString());
                 }
             });
             socket.connect();
@@ -136,23 +118,26 @@ public class MainActivity extends AppCompatActivity {
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
+    }
 
-
-
-
+    private void printMessageOnScreen(final String msg) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                logText.append(msg + "\n");
+            }
+        });
     }
 
     private void forwardCmd(String cmd) {
-        //Пытаемся послать данные
         try {
-            //Получаем выходной поток для передачи данных
             OutputStream outStream = clientSocket.getOutputStream();
-            //Пишем данные в выходной поток
             outStream.write(cmd.getBytes());
 
         } catch (IOException e) {
             //Если есть ошибки, выводим их в лог
             Log.d("BLUETOOTH", e.getMessage());
+            printMessageOnScreen("Error to send over BT cmd: " + cmd);
         }
 
     }
